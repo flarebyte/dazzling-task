6bc092eb437ebe008a6d00d3e6db87b2e7eb4bf2
olih <bitbucket.olivier.4aswabra@flarebyte.com>
2015-12-31 15:56:21 +0000

    Update dependencies

--- gulpfile.js
+++ gulpfile.js
@@ -72,5 +72,5 @@ gulp.task('clean', function () {
   return del('dist');
 });
 
-gulp.task('prepublish', ['nsp', 'babel']);
+gulp.task('prepublish', ['babel']);
 gulp.task('default', ['static', 'test', 'coveralls']);
